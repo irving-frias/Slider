04c8380d0d19e986eaa36b16ad32efc673599ffd
irving-frias <irvingfrias41@gmail.com>
2020-01-23 10:56:53 -0700

    SLIDER-4: Removed real non needed code.

--- js/scripts.js
+++ js/scripts.js
@@ -32,36 +32,3 @@ function initSlideShows() {
     }
   });
 }
-//   // Find slideshows
-//   var slideShows = $('.slider-container>div');
-// ​
-//   slideShows.each(function (ind, elem) {
-// ​
-//     var slideShowClassName = elem.className;
-//     var slideshowDashNumber = slideShowClassName.match(/cycle-slideshow-\d$/) ? slideShowClassName.match(/\d/)[0] : null;
-// ​
-//     // If it is there any slideshow parent
-//     if (slideshowDashNumber) {
-// ​
-//       var backWardButtonClassName = '.arrow-backward-' + slideshowDashNumber;
-//       var forwardButtonClassName = '.arrow-forward-' + slideshowDashNumber;
-//       var innerSlideshowName = '.inner-cycle-slideshow-' + slideshowDashNumber;
-// ​
-//       $(elem).cycle();
-//       $(innerSlideshowName).cycle();
-// ​
-//       $(backWardButtonClassName).on('click', function (ev) {
-//         ev.preventDefault();
-//         $('.' + slideShowClassName).cycle('prev');
-//         $(innerSlideshowName).cycle('prev');
-//       });
-// ​
-//       $(forwardButtonClassName).on('click', function (ev) {
-//         ev.preventDefault();
-//         $('.' + slideShowClassName).cycle('next');
-//         $(innerSlideshowName).cycle('next');
-//       });
-// ​
-//     } 
-// ​
-//   });
